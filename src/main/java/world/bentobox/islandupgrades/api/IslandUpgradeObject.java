bf98588b9f3cbc42d268ad9b5eb5c5d2ca8e2415
guillaume-lebegue <guillaume.lebegue@epitech.eu>
2020-05-21 16:11:25 +0200

    comment on use of general object

--- src/main/java/world/bentobox/islandupgrades/api/IslandUpgradeObject.java
+++ src/main/java/world/bentobox/islandupgrades/api/IslandUpgradeObject.java
@@ -13,8 +13,23 @@ import world.bentobox.bentobox.database.objects.Island;
 import world.bentobox.islandupgrades.IslandUpgradesAddon;
 import world.bentobox.islandupgrades.IslandUpgradesData;
 
+/**
+ * Upgrade Object for IslandUpgradeAddon. Extend this to create a new upgrade
+ * 
+ * @author Ikkino
+ *
+ */
 public abstract class IslandUpgradeObject {
 	
+	/**
+	 * Initialize the upgrade object
+	 * you should call it in your init methode
+	 * 
+	 * @param addon: This should be your addon
+	 * @param name: This is the name for the upgrade that will be used in the DataBase
+	 * @param displayName: This is the name that is shown to the user 
+	 * @param icon: This is the icon shown to the user
+	 */
 	public IslandUpgradeObject(Addon addon, String name, String displayName, Material icon) {
 		this.name = name;
 		this.displayName = displayName;
@@ -30,8 +45,25 @@ public abstract class IslandUpgradeObject {
 			this.islandUpgradeAddon = (IslandUpgradesAddon) islandUpgrade.get();
 	}
 	
+	/**
+	 * This fonction is called every times a user open the interface
+	 * You should make it update the upgradeValues
+	 * 
+	 * @param user: This is the user that ask for the interface
+	 * @param island: This is the island concerned by the interface
+	 */
 	public abstract void updateUpgradeValue(User user, Island island);
 	
+	/**
+	 * This function return true if the user can upgrade for this island.
+	 * You can override it and call the super.
+	 * 
+	 * The super test for islandLevel and for money
+	 * 
+	 * @param user: This is the user that try to upgrade
+	 * @param island: This is the island that is concerned
+	 * @return: Can upgrade
+	 */
 	public boolean canUpgrade(User user, Island island) {
 		boolean can = true;
 		
@@ -50,6 +82,16 @@ public abstract class IslandUpgradeObject {
 		return can;
 	}
 	
+	/**
+	 * This function is called when the user is upgrading for the island
+	 * It is called after the canUpgrade function
+	 * 
+	 * You should call the super to update the balance of the user as well as the level is the island
+	 * 
+	 * @param user: This is the user that do the upgrade
+	 * @param island: This is the island that is concerned
+	 * @return: If upgrade was successful
+	 */
 	public boolean doUpgrade(User user, Island island) {
 		
 		if (this.islandUpgradeAddon.isVaultProvided()) {
@@ -67,34 +109,61 @@ public abstract class IslandUpgradeObject {
 		return true;
 	}
 	
+	/**
+	 * @return: The name that is used for the DataBase
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * @return: The name that is displayed to the user
+	 */
 	public String getDisplayName() {
 		return this.displayName;
 	}
 	
+	/**
+	 * @param displayName: To update the name to display to the user
+	 */
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
 	
+	/**
+	 * @return: The icon that is displayed to the user
+	 */
 	public Material getIcon() {
 		return this.icon;
 	}
 	
+	/**
+	 * @return: The actual upgradeValues
+	 */
 	public UpgradeValues getUpgradeValues() {
 		return this.upgradeValues;
 	}
 	
+	/**
+	 * @param upgrade: Values to upgrades
+	 */
 	public void setUpgradeValues(UpgradeValues upgrade) {
 		this.upgradeValues = upgrade;
 	}
 	
+	/**
+	 * Function that get the IslandUpgradesAddon
+	 * You should use it to use the islandUpgradesAddon methods
+	 * 
+	 * @return
+	 */
 	public IslandUpgradesAddon getIslandUpgradeAddon() {
 		return this.islandUpgradeAddon;
 	}
 	
+	/**
+	 * You shouldn't override this function
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == this)
--- src/main/java/world/bentobox/islandupgrades/upgrades/RangeUpgrade.java
+++ src/main/java/world/bentobox/islandupgrades/upgrades/RangeUpgrade.java
@@ -11,63 +11,94 @@ import world.bentobox.islandupgrades.IslandUpgradesAddon;
 import world.bentobox.islandupgrades.IslandUpgradesData;
 import world.bentobox.islandupgrades.api.IslandUpgradeObject;
 
+/**
+ * Upgrade Object for range upgrade
+ * 
+ * @author Ikkino
+ *
+ */
 public class RangeUpgrade extends IslandUpgradeObject {
 
 	public RangeUpgrade(IslandUpgradesAddon addon) {
 		super(addon, "RangeUpgrade", "RangeUpgrade", Material.OAK_FENCE);
 	}
 
+	/**
+	 * When user open the interface
+	 */
 	@Override
 	public void updateUpgradeValue(User user, Island island) {
+		// Get the addon
 		IslandUpgradesAddon islandAddon = this.getIslandUpgradeAddon();
+		// Get the data from IslandUpgrade
 		IslandUpgradesData islandData = islandAddon.getIslandUpgradesLevel(island.getUniqueId());
+		// The level of this upgrade
 		long upgradeLevel = islandData.getUpgradeLevel(getName());
+		// The number of members on the island
 		long numberPeople = island.getMemberSet().size();
+		// The level of the island from Level Addon
 		long islandLevel;
 		
+		// If level addon is provided
 		if (islandAddon.isLevelProvided())
 			islandLevel = islandAddon.getIslandUpgradesManager().getIslandLevel(island);
 		else 
 			islandLevel = 0L;
 		
+		// Get upgrades infos of range upgrade from settings
 		Map<String, Integer> upgradeInfos = islandAddon.getIslandUpgradesManager().getRangeUpgradeInfos(upgradeLevel, islandLevel, numberPeople, island.getWorld());
 		UpgradeValues upgrade;
 		
+		// If null -> no next upgrades
 		if (upgradeInfos == null)
 			upgrade = null;
 		else
 			upgrade = new UpgradeValues(upgradeInfos.get("islandMinLevel"), upgradeInfos.get("vaultCost"), upgradeInfos.get("upgradeRange"));
 		
+		// Update the upgrade values
 		this.setUpgradeValues(upgrade);
 		
+		// Update the display name
 		String newDisplayName;
 		
-		if (upgrade == null)
+		if (upgrade == null) {
+			// No next upgrade -> lang message
 			newDisplayName = user.getTranslation("islandupgrades.ui.upgradepanel.norangeupgrade");
-		else
+		} else {
+			// get lang message
 			newDisplayName = user.getTranslation("islandupgrades.ui.upgradepanel.rangeupgrade",
 				"[rangelevel]", upgrade.getUpgradeValue().toString());
+		}
 		
 		this.setDisplayName(newDisplayName);
 	}
 	
+	/**
+	 * When user do upgrade
+	 */
 	@Override
 	public boolean doUpgrade(User user, Island island) {
+		// Get the new range
 		long newRange = island.getProtectionRange() + this.getUpgradeValues().getUpgradeValue();
 		
+		// If newRange is more than the authorized range (Config problem)
 		if (newRange > island.getRange()) {
 			this.getIslandUpgradeAddon().logWarning("User tried to upgrade their island range over the max. This is probably a configuration problem.");
 			user.sendMessage("islandupgrades.error.rangeovermax");
 			return false;
 		}
 		
+		// if super doUpgrade not worked
 		if (!super.doUpgrade(user, island))
 			return false;
 		
+		// Save oldRange for rangeChange event
 		int oldRange = island.getProtectionRange();
 		
+		// Set range
 		island.setProtectionRange((int) newRange);
 		
+		// Launch range change event
 		IslandEvent.builder()
 		.island(island)
 		.location(island.getCenter())
