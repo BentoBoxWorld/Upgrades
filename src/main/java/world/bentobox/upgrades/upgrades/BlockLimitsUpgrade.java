6be38bdfb0906daf70874d4b4ecf944960cef42c
king-glitch <58404132+king-glitch@users.noreply.github.com>
2020-11-13 02:53:23 +0700

    Added the tier name, current and max level message (#30)
    * Update en-US.yml
    
    * Added Tier name, current and max massages to gui.
    
    * Update en-US.yml
    
    change ' to ".
    
    * Update fr.yml
    
    added tier name, current and max message.
    
    * Update ja.yml
    
    added tier name, current and max message.
    
    * Update en-US.yml
    
    * Update ja.yml
    
    * Update fr.yml
    
    * Update en-US.yml
    
    Co-authored-by: king-glitch <tiger2.yaowawit@gmail.com>

--- src/main/java/world/bentobox/upgrades/upgrades/BlockLimitsUpgrade.java
+++ src/main/java/world/bentobox/upgrades/upgrades/BlockLimitsUpgrade.java
@@ -10,7 +10,6 @@ import org.bukkit.permissions.PermissionAttachmentInfo;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.objects.Island;
 
-
 import world.bentobox.limits.listeners.BlockLimitsListener;
 import world.bentobox.upgrades.UpgradesAddon;
 import world.bentobox.upgrades.api.Upgrade;
@@ -22,7 +21,7 @@ public class BlockLimitsUpgrade extends Upgrade {
 		super(addon, "LimitsUpgrade-" + block.toString(), block.toString() + " limits Upgrade", block);
 		this.block = block;
 	}
-	
+
 	@Override
 	public void updateUpgradeValue(User user, Island island) {
 		UpgradesAddon upgradeAddon = this.getUpgradesAddon();
@@ -30,36 +29,38 @@ public class BlockLimitsUpgrade extends Upgrade {
 		int upgradeLevel = islandData.getUpgradeLevel(getName());
 		int numberPeople = island.getMemberSet().size();
 		int islandLevel = upgradeAddon.getUpgradesManager().getIslandLevel(island);
-		
-		Map<String, Integer> upgradeInfos = upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeInfos(this.block, upgradeLevel, islandLevel, numberPeople, island.getWorld());
+
+		Map<String, Integer> upgradeInfos = upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeInfos(this.block,
+				upgradeLevel, islandLevel, numberPeople, island.getWorld());
 		UpgradeValues upgrade;
-		
+
 		// Get new description
-		String description = upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeTierName(this.block, upgradeLevel, island.getWorld()) + " (" + upgradeLevel + "/" +
-							upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeMax(this.block, island.getWorld()) + ")";
+		String description =  user.getTranslation("upgrades.ui.upgradepanel.tiernameandlevel", "[name]", upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeTierName(this.block, upgradeLevel,
+				island.getWorld()), "[current]", Integer.toString(upgradeLevel), "[max]", Integer.toString(upgradeAddon.getUpgradesManager().getBlockLimitsUpgradeMax(this.block, island.getWorld())));
 		// Set new description
 		this.setOwnDescription(user, description);
-		
+
 		if (upgradeInfos == null)
 			upgrade = null;
 		else
-			upgrade = new UpgradeValues(upgradeInfos.get("islandMinLevel"), upgradeInfos.get("vaultCost"), upgradeInfos.get("upgrade"));
-		
+			upgrade = new UpgradeValues(upgradeInfos.get("islandMinLevel"), upgradeInfos.get("vaultCost"),
+					upgradeInfos.get("upgrade"));
+
 		this.setUpgradeValues(user, upgrade);
-		
+
 		String newDisplayName;
-		
+
 		if (upgrade == null) {
-			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.nolimitsupgrade",
-				"[block]", this.block.toString());
+			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.nolimitsupgrade", "[block]",
+					this.block.toString());
 		} else {
-			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.limitsupgrade",
-				"[block]", this.block.toString(), "[level]", Integer.toString(upgrade.getUpgradeValue()));
+			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.limitsupgrade", "[block]",
+					this.block.toString(), "[level]", Integer.toString(upgrade.getUpgradeValue()));
 		}
-		
+
 		this.setDisplayName(newDisplayName);
 	}
-	
+
 	@Override
 	public boolean isShowed(User user, Island island) {
 		// Get the addon
@@ -69,81 +70,84 @@ public class BlockLimitsUpgrade extends Upgrade {
 		// Get level of the upgrade
 		int upgradeLevel = islandData.getUpgradeLevel(this.getName());
 		// Permission level required
-		int permissionLevel = upgradesAddon.getUpgradesManager().getBlockLimitsPermissionLevel(this.block, upgradeLevel, island.getWorld());
-		
+		int permissionLevel = upgradesAddon.getUpgradesManager().getBlockLimitsPermissionLevel(this.block, upgradeLevel,
+				island.getWorld());
+
 		// If default permission, then true
 		if (permissionLevel == 0)
 			return true;
-		
+
 		Player player = user.getPlayer();
 		String gamemode = island.getGameMode();
 		String permissionStart = gamemode + ".upgrades." + this.getName() + ".";
 		permissionStart = permissionStart.toLowerCase();
-		
+
 		// For each permission of the player
 		for (PermissionAttachmentInfo perms : player.getEffectivePermissions()) {
-			
-			// If permission is the one we search 
+
+			// If permission is the one we search
 			if (!perms.getValue() || !perms.getPermission().startsWith(permissionStart))
 				continue;
-			
+
 			if (perms.getPermission().contains(permissionStart + "*")) {
 				this.logError(player.getName(), perms.getPermission(), "Wildcards are not allowed.");
 				return false;
 			}
-			
+
 			String[] split = perms.getPermission().split("\\.");
 			if (split.length != 4) {
 				logError(player.getName(), perms.getPermission(), "format must be '" + permissionStart + "LEVEL'");
 				return false;
 			}
-			
+
 			if (!NumberUtils.isDigits(split[3])) {
 				logError(player.getName(), perms.getPermission(), "The last part must be a number");
 				return false;
 			}
-			
+
 			if (permissionLevel <= Integer.parseInt(split[3]))
 				return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private void logError(String name, String perm, String error) {
-        this.getUpgradesAddon().logError("Player " + name + " has permission: '" + perm + "' but " + error + " Ignoring...");
-    }
-	
+		this.getUpgradesAddon()
+				.logError("Player " + name + " has permission: '" + perm + "' but " + error + " Ignoring...");
+	}
+
 	@Override
 	public boolean doUpgrade(User user, Island island) {
 		UpgradesAddon islandAddon = this.getUpgradesAddon();
-		
+
 		if (!islandAddon.isLimitsProvided())
 			return false;
-		
+
 		BlockLimitsListener bLListener = islandAddon.getLimitsAddon().getBlockLimitListener();
 		Map<Material, Integer> materialLimits = bLListener.getMaterialLimits(island.getWorld(), island.getUniqueId());
-		
+
 		if (!materialLimits.containsKey(this.block) || materialLimits.get(this.block) == -1) {
-			this.getUpgradesAddon().logWarning("User tried to upgrade " + this.block.toString() + " limits but it has no limits. This is probably a configuration problem.");
+			this.getUpgradesAddon().logWarning("User tried to upgrade " + this.block.toString()
+					+ " limits but it has no limits. This is probably a configuration problem.");
 			user.sendMessage("upgrades.error.increasenolimits");
 			return false;
 		}
-		
+
 		if (!super.doUpgrade(user, island))
 			return false;
-		
-		int oldCount = materialLimits.get(this.block); 
+
+		int oldCount = materialLimits.get(this.block);
 		int newCount = (int) (oldCount + this.getUpgradeValues(user).getUpgradeValue());
-		
+
 		bLListener.getIsland(island.getUniqueId()).setBlockLimit(this.block, newCount);
-		
-		user.sendMessage("upgrades.ui.upgradepanel.limitsupgradedone",
-			"[block]", this.block.toString(), "[level]", Integer.toString(this.getUpgradeValues(user).getUpgradeValue()));
-		
+
+		user.sendMessage("upgrades.ui.upgradepanel.limitsupgradedone", "[block]", this.block.toString(), "[level]",
+				Integer.toString(this.getUpgradeValues(user).getUpgradeValue()));
+
 		return true;
 	}
-	
+
 	private Material block;
-	
+
 }
--- src/main/java/world/bentobox/upgrades/upgrades/EntityLimitsUpgrade.java
+++ src/main/java/world/bentobox/upgrades/upgrades/EntityLimitsUpgrade.java
@@ -15,12 +15,13 @@ import world.bentobox.upgrades.api.Upgrade;
 import world.bentobox.upgrades.dataobjects.UpgradesData;
 
 public class EntityLimitsUpgrade extends Upgrade {
-	
+
 	public EntityLimitsUpgrade(UpgradesAddon addon, EntityType entity) {
-		super(addon, "LimitsUpgrade-" + entity.toString(), entity.toString() + " limits Upgrade", addon.getSettings().getEntityIcon(entity));
+		super(addon, "LimitsUpgrade-" + entity.toString(), entity.toString() + " limits Upgrade",
+				addon.getSettings().getEntityIcon(entity));
 		this.entity = entity;
 	}
-	
+
 	@Override
 	public void updateUpgradeValue(User user, Island island) {
 		UpgradesAddon upgradeAddon = this.getUpgradesAddon();
@@ -28,41 +29,42 @@ public class EntityLimitsUpgrade extends Upgrade {
 		int upgradeLevel = islandData.getUpgradeLevel(this.getName());
 		int numberPeople = island.getMemberSet().size();
 		int islandLevel;
-		
+
 		if (upgradeAddon.isLevelProvided())
 			islandLevel = upgradeAddon.getUpgradesManager().getIslandLevel(island);
 		else
 			islandLevel = 0;
-		
-		Map<String, Integer> upgradeInfos = upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeInfos(this.entity, upgradeLevel, islandLevel, numberPeople, island.getWorld());
+
+		Map<String, Integer> upgradeInfos = upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeInfos(this.entity,
+				upgradeLevel, islandLevel, numberPeople, island.getWorld());
 		UpgradeValues upgrade;
-		
 		// Get new description
-		String description = upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeTierName(this.entity, upgradeLevel, island.getWorld()) + " (" + upgradeLevel + "/" +
-							upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeMax(this.entity, island.getWorld()) + ")";
+		String description = user.getTranslation("upgrades.ui.upgradepanel.tiernameandlevel", "[name]", upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeTierName(this.entity, upgradeLevel,
+				island.getWorld()), "[current]", Integer.toString(upgradeLevel), "[max]", Integer.toString(upgradeAddon.getUpgradesManager().getEntityLimitsUpgradeMax(this.entity, island.getWorld())));
 		// Set new description
 		this.setOwnDescription(user, description);
-		
+
 		if (upgradeInfos == null)
 			upgrade = null;
 		else
-			upgrade = new UpgradeValues(upgradeInfos.get("islandMinLevel"), upgradeInfos.get("vaultCost"), upgradeInfos.get("upgrade"));
-		
+			upgrade = new UpgradeValues(upgradeInfos.get("islandMinLevel"), upgradeInfos.get("vaultCost"),
+					upgradeInfos.get("upgrade"));
+
 		this.setUpgradeValues(user, upgrade);
-		
+
 		String newDisplayName;
-		
+
 		if (upgrade == null) {
-			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.nolimitsupgrade",
-				"[block]", this.entity.toString());
+			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.nolimitsupgrade", "[block]",
+					this.entity.toString());
 		} else {
-			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.limitsupgrade",
-				"[block]", this.entity.toString(), "[level]", Integer.toString(upgrade.getUpgradeValue()));
+			newDisplayName = user.getTranslation("upgrades.ui.upgradepanel.limitsupgrade", "[block]",
+					this.entity.toString(), "[level]", Integer.toString(upgrade.getUpgradeValue()));
 		}
-		
+
 		this.setDisplayName(newDisplayName);
 	}
-	
+
 	@Override
 	public boolean isShowed(User user, Island island) {
 		// Get the addon
@@ -72,80 +74,83 @@ public class EntityLimitsUpgrade extends Upgrade {
 		// Get level of the upgrade
 		int upgradeLevel = islandData.getUpgradeLevel(this.getName());
 		// Permission level required
-		int permissionLevel = upgradesAddon.getUpgradesManager().getEntityLimitsPermissionLevel(this.entity, upgradeLevel, island.getWorld());
-		
+		int permissionLevel = upgradesAddon.getUpgradesManager().getEntityLimitsPermissionLevel(this.entity,
+				upgradeLevel, island.getWorld());
+
 		// If default permission, then true
 		if (permissionLevel == 0)
 			return true;
-		
+
 		Player player = user.getPlayer();
 		String gamemode = island.getGameMode();
-		String permissionStart = gamemode + ".upgrades." + this.getName() + "."; 
+		String permissionStart = gamemode + ".upgrades." + this.getName() + ".";
 		permissionStart = permissionStart.toLowerCase();
-		
+
 		// For each permission of the player
 		for (PermissionAttachmentInfo perms : player.getEffectivePermissions()) {
-			
-			// If permission is the one we search 
+
+			// If permission is the one we search
 			if (!perms.getValue() || !perms.getPermission().startsWith(permissionStart))
 				continue;
-			
+
 			if (perms.getPermission().contains(permissionStart + "*")) {
 				this.logError(player.getName(), perms.getPermission(), "Wildcards are not allowed.");
 				return false;
 			}
-			
+
 			String[] split = perms.getPermission().split("\\.");
 			if (split.length != 4) {
 				logError(player.getName(), perms.getPermission(), "format must be '" + permissionStart + "LEVEL'");
 				return false;
 			}
-			
+
 			if (!NumberUtils.isDigits(split[3])) {
 				logError(player.getName(), perms.getPermission(), "The last part must be a number");
 				return false;
 			}
-			
+
 			if (permissionLevel <= Integer.parseInt(split[3]))
 				return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private void logError(String name, String perm, String error) {
-        this.getUpgradesAddon().logError("Player " + name + " has permission: '" + perm + "' but " + error + " Ignoring...");
-    }
-	
+		this.getUpgradesAddon()
+				.logError("Player " + name + " has permission: '" + perm + "' but " + error + " Ignoring...");
+	}
+
 	@Override
 	public boolean doUpgrade(User user, Island island) {
 		UpgradesAddon islandAddon = this.getUpgradesAddon();
-		
+
 		if (!islandAddon.isLimitsProvided())
 			return false;
-		
+
 		BlockLimitsListener bLListener = islandAddon.getLimitsAddon().getBlockLimitListener();
 		Map<EntityType, Integer> entityLimits = islandAddon.getUpgradesManager().getEntityLimits(island);
-		
+
 		if (!entityLimits.containsKey(this.entity) || entityLimits.get(this.entity) == -1) {
-			this.getUpgradesAddon().logWarning("User tried to upgrade " + this.entity.toString() + " limits but it has no limits. This is probably a configuration problem.");
+			this.getUpgradesAddon().logWarning("User tried to upgrade " + this.entity.toString()
+					+ " limits but it has no limits. This is probably a configuration problem.");
 			user.sendMessage("upgrades.error.increasenolimits");
 			return false;
 		}
-		
+
 		if (!super.doUpgrade(user, island))
 			return false;
-		 
+
 		int newCount = (int) (entityLimits.get(this.entity) + this.getUpgradeValues(user).getUpgradeValue());
-		
+
 		bLListener.getIsland(island.getUniqueId()).setEntityLimit(this.entity, newCount);
-		
-		user.sendMessage("upgrades.ui.upgradepanel.limitsupgradedone",
-			"[block]", this.entity.toString(), "[level]", Integer.toString(this.getUpgradeValues(user).getUpgradeValue()));
-		
+
+		user.sendMessage("upgrades.ui.upgradepanel.limitsupgradedone", "[block]", this.entity.toString(), "[level]",
+				Integer.toString(this.getUpgradeValues(user).getUpgradeValue()));
+
 		return true;
 	}
-	
+
 	private EntityType entity;
 
 }
