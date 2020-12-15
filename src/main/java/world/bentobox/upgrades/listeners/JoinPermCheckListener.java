47cde576ba16efc1575fdb12c6acb29a18d72cf1
tastybento <tastybento@wasteofplastic.com>
2020-12-14 23:04:56 -0800

    Optimized JoinPermCheckListener
    Merged a number of collapsible if statements to increase the code's
    readability. Made fields final.

--- src/main/java/world/bentobox/upgrades/listeners/JoinPermCheckListener.java
+++ src/main/java/world/bentobox/upgrades/listeners/JoinPermCheckListener.java
@@ -10,39 +10,31 @@ import org.bukkit.event.Listener;
 import world.bentobox.limits.Settings.EntityGroup;
 import world.bentobox.limits.events.LimitsPermCheckEvent;
 import world.bentobox.upgrades.UpgradesAddon;
+import world.bentobox.upgrades.UpgradesManager;
 
 public class JoinPermCheckListener implements Listener {
 
-	public JoinPermCheckListener(UpgradesAddon addon) {
-		this.addon = addon;
-	}
-	
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
-	public void onLimitsPermCheckEvent(LimitsPermCheckEvent e) {
-		Material block = e.getMaterial();
-		EntityType et = e.getEntityType();
-		EntityGroup entgroup = e.getEntityGroup();
-		World world = e.getPlayer().getWorld();
-		
-		if (block != null) {
-			if (this.addon.getUpgradesManager().getAllBlockLimitsUpgradeTiers(world).containsKey(block)) {
-				e.setCancelled(true);
-			}
-		}
-		
-		if (et != null) {
-			if (this.addon.getUpgradesManager().getAllEntityLimitsUpgradeTiers(world).containsKey(et)) {
-				e.setCancelled(true);
-			}
-		}
-		
-		if (entgroup != null) {
-			if (this.addon.getUpgradesManager().getAllEntityGroupLimitsUpgradeTiers(world).containsKey(entgroup.getName())) {
-				e.setCancelled(true);
-			}
-		}
-	}
-	
-	UpgradesAddon addon;
-	
+    private final UpgradesAddon addon;
+    private final UpgradesManager m;
+
+    public JoinPermCheckListener(UpgradesAddon addon) {
+        this.addon = addon;
+        this.m = this.addon.getUpgradesManager();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void onLimitsPermCheckEvent(LimitsPermCheckEvent e) {
+        Material block = e.getMaterial();
+        EntityType et = e.getEntityType();
+        EntityGroup entgroup = e.getEntityGroup();
+        World world = e.getPlayer().getWorld();
+
+        e.setCancelled((
+                (block != null && m.getAllBlockLimitsUpgradeTiers(world).containsKey(block)) ||
+                (et != null && m.getAllEntityLimitsUpgradeTiers(world).containsKey(et)) ||
+                (entgroup != null && m.getAllEntityGroupLimitsUpgradeTiers(world).containsKey(entgroup.getName())))
+                );
+
+    }
+
 }
