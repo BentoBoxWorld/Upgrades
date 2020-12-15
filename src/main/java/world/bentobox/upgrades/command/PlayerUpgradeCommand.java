06dd199525b91be2e77188979db64778a406618b
tastybento <tastybento@wasteofplastic.com>
2020-12-14 22:53:59 -0800

    Used isEmpty() to check whether the collection is empty or not.

--- src/main/java/world/bentobox/upgrades/command/PlayerUpgradeCommand.java
+++ src/main/java/world/bentobox/upgrades/command/PlayerUpgradeCommand.java
@@ -11,64 +11,64 @@ import world.bentobox.upgrades.ui.Panel;
 
 public class PlayerUpgradeCommand extends CompositeCommand {
 
-	public PlayerUpgradeCommand(UpgradesAddon addon, CompositeCommand cmd) {
-		super(addon, cmd, "upgrade");
-		
-		this.addon = addon;
-	}
-	
-	@Override
-	public void setup() {
-		this.setDescription("upgrades.commands.main.description");
-		this.setOnlyPlayer(true);
-	}
-	
-	@Override
-	public boolean canExecute(User user, String label, List<String> args) {
-		Island island = getIslands().getIsland(this.getWorld(), user);
-		
-		if (island == null) {
-			user.sendMessage("general.errors.no-island");
-			return false;
-		}
-		
-		if (!island.onIsland(user.getLocation())) {
-			user.sendMessage("upgrades.error.notonisland");
-			return false;
-		}
-		
-		if (!island.isAllowed(user, UpgradesAddon.UPGRADES_RANK_RIGHT)) {
-			user.sendMessage("general.errors.insufficient-rank",
-				TextVariables.RANK,
-				user.getTranslation(this.addon.getPlugin().getRanksManager().getRank(island.getRank(user))));
-			return false;
-		}
-		
-		return true;
-	}
-	
-	@Override
-	public boolean execute(User user, String label, List<String> args) {
-		if (args.size() == 0) {
-			Island island = getIslands().getIsland(this.getWorld(), user); 
-			
-			if (island == null) {
-				user.sendMessage("general.errors.no-island");
-				return false;
-			}
-			
-			if (!island.onIsland(user.getLocation())) {
-				user.sendMessage("upgrades.error.notonisland");
-				return false;
-			}
-			
-			new Panel(this.addon, island).showPanel(user);
-			return true;
-		}
-		this.showHelp(this, user);
-		return false;
-	}
-	
-	UpgradesAddon addon;
-	
+    public PlayerUpgradeCommand(UpgradesAddon addon, CompositeCommand cmd) {
+        super(addon, cmd, "upgrade");
+
+        this.addon = addon;
+    }
+
+    @Override
+    public void setup() {
+        this.setDescription("upgrades.commands.main.description");
+        this.setOnlyPlayer(true);
+    }
+
+    @Override
+    public boolean canExecute(User user, String label, List<String> args) {
+        Island island = getIslands().getIsland(this.getWorld(), user);
+
+        if (island == null) {
+            user.sendMessage("general.errors.no-island");
+            return false;
+        }
+
+        if (!island.onIsland(user.getLocation())) {
+            user.sendMessage("upgrades.error.notonisland");
+            return false;
+        }
+
+        if (!island.isAllowed(user, UpgradesAddon.UPGRADES_RANK_RIGHT)) {
+            user.sendMessage("general.errors.insufficient-rank",
+                    TextVariables.RANK,
+                    user.getTranslation(this.addon.getPlugin().getRanksManager().getRank(island.getRank(user))));
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean execute(User user, String label, List<String> args) {
+        if (args.isEmpty()) {
+            Island island = getIslands().getIsland(this.getWorld(), user);
+
+            if (island == null) {
+                user.sendMessage("general.errors.no-island");
+                return false;
+            }
+
+            if (!island.onIsland(user.getLocation())) {
+                user.sendMessage("upgrades.error.notonisland");
+                return false;
+            }
+
+            new Panel(this.addon, island).showPanel(user);
+            return true;
+        }
+        this.showHelp(this, user);
+        return false;
+    }
+
+    UpgradesAddon addon;
+
 }
